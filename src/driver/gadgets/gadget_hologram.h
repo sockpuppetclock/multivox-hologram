2f1a339e7673c46db8d6fefb81d12e64063857a8
sockpuppetclock <42406458+sockpuppetclock@users.noreply.github.com>
2025-04-18 13:03:44 -0500

    fixed missing row gpio

--- src/driver/gadgets/gadget_hologram.h
+++ src/driver/gadgets/gadget_hologram.h
@@ -3,7 +3,7 @@
 
 // generic HUB75 64x64
 
-#define SPIN_SYNC 1
+#define SPIN_SYNC 3
 
 #define RGB_0_B1 7
 #define RGB_0_G1 27
@@ -19,6 +19,13 @@
 #define RGB_1_G2 13
 #define RGB_1_R2 19
 
+#define ROW_A 22
+#define ROW_B 23
+#define ROW_C 24
+#define ROW_D 25
+#define ROW_E 1
+#define ROW_MASK ((1<<ROW_A)|(1<<ROW_B)|(1<<ROW_C)|(1<<ROW_D)|(1<<ROW_E))
+
 #define RGB_BLANK 18 // OE
 #define RGB_CLOCK 17
 #define RGB_STROBE 4
@@ -30,7 +37,7 @@
 #define RGB_1_MASK ((1<<RGB_1_R1)|(1<<RGB_1_G1)|(1<<RGB_1_B1)|(1<<RGB_1_R2)|(1<<RGB_1_G2)|(1<<RGB_1_B2))
 #define RGB_BITS_MASK (RGB_0_MASK | RGB_1_MASK)
 
-static const int matrix_init_out[] = {RGB_0_B1, RGB_0_G1, RGB_0_R1, RGB_0_B2, RGB_0_G2, RGB_0_R2, RGB_1_B1, RGB_1_G1, RGB_1_R1, RGB_1_B2, RGB_1_G2, RGB_1_R2, RGB_BLANK, RGB_CLOCK, RGB_STROBE};
+static const int matrix_init_out[] = {RGB_0_B1, RGB_0_G1, RGB_0_R1, RGB_0_B2, RGB_0_G2, RGB_0_R2, RGB_1_B1, RGB_1_G1, RGB_1_R1, RGB_1_B2, RGB_1_G2, RGB_1_R2, ROW_A, ROW_B, ROW_C, ROW_D, ROW_E, RGB_BLANK, RGB_CLOCK, RGB_STROBE};
 
 #define PANEL_WIDTH  64
 #define PANEL_HEIGHT 64
